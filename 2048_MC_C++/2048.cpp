d5f7353aa382ba548467db6caa8c9932184a1627
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-01-24 23:12:08 -0600

    Implemented  merge function

--- 2048_MC_C++/2048.cpp
+++ 2048_MC_C++/2048.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 class game{
-	int score;
+	int score, tile;
 	bool lose;
 	vector<vector<int> > board;
 
 public:
-	game(): score(0), lose(false), board(4,vector<int>(4)){
+	game(): score(0), tile(0), lose(false), board(4,vector<int>(4)){
 		//Initialize board with a 2 at a random location
 		int i = rand()%4;
 		int j = rand()%4;
@@ -34,6 +34,8 @@ public:
                 cout<<"}\n";
 	        }
 	}
+
+
 private:
 	void shift(){
 		vector<vector<int> > current(4, vector<int>(4));
@@ -48,6 +50,22 @@ private:
 		}
 		board = current;
 	}
+	void merge(){
+		shift();
+		for (int i=0; i<4; i++){
+			for (int j=0; j<3; j++){
+				if (board[i][j] == board[i][j+1] and board[i][j]!=0){
+					board[i][j] *=2;
+					score+= board[i][j];
+					if (board[i][j] > tile)
+						tile = board[i][j];
+					board[i][j+1] = 0;
+				}
+			}
+		}
+		shift();
+	}
+
 		
 };
 
@@ -58,4 +76,6 @@ int main(){
 	cout<<example.get_score()<<endl;
 	cout<<example.get_lost()<<endl;
 	example.print_board();
+//	example.merge();
+//	example.print_board();
 }
